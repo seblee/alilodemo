9a08e727b0f7af1e34f67d3bedb051c24a892a3f
Yisa Lee <ceeu@vip.qq.com>
2017-06-19 09:17:35 +0800

    局域网httpServer的URL改为外网的URL

--- testdemo/audio_test.h
+++ testdemo/audio_test.h
@@ -1,7 +1,7 @@
 #ifndef _AUDIO_TEST_H
 #define _AUDIO_TEST_H
 
-#define URL_FILE_DNLD   "http://192.168.3.91:12345/1.mp3"
+#define URL_FILE_DNLD   "http://fdfs.xmcdn.com/group21/M00/84/14/wKgJLVg_jIPRlRj0AEncvwvmmWc779.mp3"
                         //"http://192.168.31.108:12345/1.mp3"
                         //"http://fdfs.xmcdn.com/group21/M00/84/14/wKgJLVg_jIPRlRj0AEncvwvmmWc779.mp3"
                         //"http://192.168.3.91:12345/1.mp3"
